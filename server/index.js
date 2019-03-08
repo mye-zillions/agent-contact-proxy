62e333787b599f67e5229e96042617ae53d25b44
Elizabeth Cannon <elizabeth230@gmail.com>
2019-03-07 19:00:12 -0800

    Hook up proxy

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 const port = 8888;
 
 app.use(morgan('dev'));
-app.use('/:id', express.static('client'));
 
-app.listen(port);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/:houseId', express.static('client'));
+
+app.use('/api/houses/', proxy({target: 'http://localhost:8081', changeOrigin : true}))
+
+app.use('/api/agents', proxy({target: 'http://localhost:8081', changeOrigin : true}));
+
+app.listen(port, () => {
+  console.log(`Hidey-Ho Cap'n, we are now serving on port ${port}!`);
+});
